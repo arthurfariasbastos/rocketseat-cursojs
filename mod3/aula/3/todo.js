742f4977dbddb4452bee7acdd55a7214ab6caa20
unknown <arthur_bastos@estudante.sc.senai.br>
2019-06-28 20:09:53 -0300

    Aula 3, Mod 3

--- mod3/aula/3/todo.js
+++ mod3/aula/3/todo.js
@@ -1,7 +1,21 @@
-var listElement
-document.querySelector("#app ul");
-var inputElement
-document.querySelector("#app input");
+var listElement = document.querySelector("#app ul");
+var inputElement = document.querySelector("#app input");
+var buttonElement = document.querySelector("#app buttob");
 
-console.log(listElement);
-console.log(inputElement);
+var todos = [
+    "VisitarSogra",
+    "Fazer um beat hj",
+    "Deveres"
+]
+
+function renderTodos() {
+    for (todo of todos){
+        var todoElement = createElement(li);
+        var todoText = createTextNode(todo);
+
+        todoElement.appendChild(todoText);
+        listElement.appendChild(todoElement);
+    }
+}
+
+renderTodos(); 
