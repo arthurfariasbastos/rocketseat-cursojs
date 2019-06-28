7d298f69e098e69f525547cf2c14349a7cf8b86f
unknown <arthur_bastos@estudante.sc.senai.br>
2019-06-28 20:10:55 -0300

    Aula 2, Mod 3  ATUALIZADA

--- mod3/aula/2/todo.js
+++ mod3/aula/2/todo.js
@@ -1,6 +1,9 @@
 var listElement = document.querySelector("#app ul");
+
 var inputElement = document.querySelector("#app input");
+
 var buttonElement = document.querySelector("#app buttob");
+
 
 var todos = [
     "VisitarSogra",
